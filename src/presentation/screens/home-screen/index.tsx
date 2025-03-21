baf3f286b83bbfbfc60719ceb99b379955861772
Anshul <anshul.sharma3@cars24.com>
2025-03-22 04:42:24 +0530

    driving stats card

--- src/presentation/screens/home-screen/index.tsx
+++ src/presentation/screens/home-screen/index.tsx
@@ -1,11 +1,12 @@
 import React, { memo } from 'react';
-import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../../navigation/RootStack';
 import ScreenWrapper from '../../widgets/screen-wrapper';
 import HomePageHeader from '../../../components/homepage-header';
 import HomePageMetricContainer from '../../../components/homepage-metric-container';
 import LiveTrackingContainer from '../../../components/live-tracking';
+import DrivingStatsCard from '../../../components/driving-stats-card';
 
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'MainTabs'>;
 type Props = { navigation: HomeScreenNavigationProp };
@@ -16,6 +17,7 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
       <ScrollView contentContainerStyle={{flex:1}}>
         <HomePageHeader />
         <HomePageMetricContainer />
+        <DrivingStatsCard />
         {/* <LiveTrackingContainer /> */}
       </ScrollView>
     </ScreenWrapper>
