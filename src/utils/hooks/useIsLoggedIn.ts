4f94ea74b04decfe6ab302ec4dc20b4aeb342942
Manish Trivedi <manish.trivedi1@cars24.com>
2025-03-22 03:16:27 +0530

    made isLogged true

--- src/utils/hooks/useIsLoggedIn.ts
+++ src/utils/hooks/useIsLoggedIn.ts
@@ -2,6 +2,6 @@ import {useSelector} from 'react-redux';
 
 export const useIsLoggedIn = () => {
   //   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  const isLoggedIn = false;
+  const isLoggedIn = true;
   return isLoggedIn;
 };
