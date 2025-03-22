e80c75d5ac501215a465f3b8777735758e7d7537
Manish Trivedi <manish.trivedi1@cars24.com>
2025-03-22 19:09:01 +0530

    auth flow

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import React, {useState} from 'react';
 import {Provider} from 'react-redux';
-import store from './src/store/store';
+import store, {persistor} from './src/store/store';
 import AppNavigator from './src/navigation/AppNavigator';
 import ErrorBoundary from './src/components/error-boundry';
 import SplashScreen from './src/presentation/screens/splash-screen';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   const [isSplashVisible, setIsSplashVisible] = useState(true);
 
   return (
     <Provider store={store}>
-      <ErrorBoundary>
-        {isSplashVisible ? (
-          <SplashScreen onFinish={() => setIsSplashVisible(false)} />
-        ) : (
-          <AppNavigator />
-        )}
-      </ErrorBoundary>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          {isSplashVisible ? (
+            <SplashScreen onFinish={() => setIsSplashVisible(false)} />
+          ) : (
+            <AppNavigator />
+          )}
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   );
 };
