3bb80eb8d0d67b2a878bac4c1efb8a0008657851
Anshul <anshul.sharma3@cars24.com>
2025-03-22 14:42:59 +0530

    app wrapped in error boundry

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/store/store'; // Import the Redux store
 import AppNavigator from './src/navigation/AppNavigator'; // Import your navigation component
+import ErrorBoundary from './src/components/error-boundry';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <ErrorBoundary>
+       <AppNavigator />
+      </ErrorBoundary>
     </Provider>
   );
 };
